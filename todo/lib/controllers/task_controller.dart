2976c8cd30cf7ff652e256dc888fb07f5d18cb50
Mehranxt <132151347+mehranext@users.noreply.github.com>
2023-05-04 21:26:32 +0430

    make all controller services

--- todo/lib/controllers/task_controller.dart
+++ todo/lib/controllers/task_controller.dart
@@ -1,9 +1,45 @@
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:todo/models/task_model.dart';
 
 class TaskController extends GetxController {
-  void readTasks() {}
+  List<TaskModel> readTasks() {
+    var storage = GetStorage();
 
-  void addTask() {}
+    List<TaskModel> tasks = [];
 
-  void updateTask() {}
+    var keys = storage.getKeys();
+    for (var key in keys) {
+      var task = storage.read(key);
+      if (task != null) {
+        tasks.add(task);
+      }
+    }
+
+    return tasks;
+  }
+
+  bool addTask(TaskModel task) {
+    try {
+      var storage = GetStorage();
+      storage.write(task.id, task);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  bool updateTask(TaskModel task) {
+    try {
+      var storage = GetStorage();
+      var exist = storage.read(task.id);
+      if (exist != null) {
+        storage.remove(task.id);
+      }
+      storage.write(task.id, task);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
 }
