1b9cf61327f9b6b10495ec8c80eb132737719690
Mehranxt <132151347+mehranext@users.noreply.github.com>
2023-05-04 21:33:34 +0430

    create list of tasks

--- todo/lib/pages/home.dart
+++ todo/lib/pages/home.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:todo/controllers/task_controller.dart';
+import 'package:todo/models/task_model.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  HomeScreen({Key? key}) : super(key: key);
+
+  RxList<TaskModel> tasks = <TaskModel>[].obs;
+
+  //? Controller
+  final taskController = Get.find<TaskController>();
 
   @override
   Widget build(BuildContext context) {
+    _loadTasks();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Task Todo App'),
@@ -15,10 +23,46 @@ class HomeScreen extends StatelessWidget {
         onPressed: _addTask,
         child: const Icon(Icons.add),
       ),
+      body: Container(
+        margin: const EdgeInsets.all(10),
+        child: Obx(
+          () => ListView.builder(
+            itemCount: tasks.length,
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              var task = tasks.value[index];
+              return Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.grey[200],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      task.title,
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    Text(
+                      task.createDate.toString(),
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+      ),
     );
   }
 
   void _addTask() {
     Get.toNamed('/upsertTask');
+  }
+
+  void _loadTasks() {
+    tasks.value = taskController.readTasks();
   }
 }
