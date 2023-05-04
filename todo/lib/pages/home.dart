929e4ef2e4d468f2e37983bdc3e4b88959092d29
Mehranxt <132151347+mehranext@users.noreply.github.com>
2023-05-04 21:11:54 +0430

    create upsert screen and go to that

--- todo/lib/pages/home.dart
+++ todo/lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,6 +11,14 @@ class HomeScreen extends StatelessWidget {
         title: const Text('Task Todo App'),
         centerTitle: true,
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addTask,
+        child: const Icon(Icons.add),
+      ),
     );
+  }
+
+  void _addTask() {
+    Get.toNamed('/upsertTask');
   }
 }
--- todo/lib/pages/upsert.dart
+++ todo/lib/pages/upsert.dart
@@ -1,10 +1,72 @@
 import 'package:flutter/material.dart';
 
 class UpsertScreen extends StatelessWidget {
-  const UpsertScreen({Key? key}) : super(key: key);
+  UpsertScreen({Key? key}) : super(key: key);
+
+  final titleText = TextEditingController();
+  final messageText = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Insert new Task'),
+        centerTitle: true,
+      ),
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            TextField(
+              controller: titleText,
+              decoration: const InputDecoration(
+                label: Text('Title'),
+                hintText: 'title',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            TextField(
+              controller: titleText,
+              maxLines: 5,
+              keyboardType: TextInputType.multiline,
+              decoration: const InputDecoration(
+                label: Text('Text'),
+                hintText: 'Write your text',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all(
+                    const EdgeInsets.all(10),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text('Submit Task'),
+                    Icon(Icons.check),
+                  ],
+                ))
+          ],
+        ),
+      ),
+    );
   }
 }
