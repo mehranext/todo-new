7cb3a9a702331770037d1c084a848fdd93738f16
Mehranxt <132151347+mehranext@users.noreply.github.com>
2023-05-04 20:39:53 +0430

    create app
    1. create app
    2. create storage
    3. add injections

---
+++ todo/lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp();
+  }
+}
--- todo/lib/routes/router.dart
+++ todo/lib/routes/router.dart
@@ -1,8 +1,17 @@
 import 'package:get/get.dart';
+import 'package:todo/bindings/task_bindings.dart';
 import 'package:todo/pages/home.dart';
 import 'package:todo/pages/upsert.dart';
 
 var routes = [
-  GetPage(name: 'home', page: () => HomeScreen()),
-  GetPage(name: 'upsertTask', page: () => UpsertScreen()),
+  GetPage(
+    name: 'home',
+    page: () => HomeScreen(),
+    binding: TaskBinding(),
+  ),
+  GetPage(
+    name: 'upsertTask',
+    page: () => UpsertScreen(),
+    binding: TaskBinding(),
+  ),
 ];
